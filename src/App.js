bdf71fcb55865b314995c73030cce0b452b1d5f5
unknown <nbatre1@yahoo.com>
2019-10-02 22:44:25 -0500

    keep moving forward

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import {FoodDialog} from './Components/FoodDialog';
 import Order from './Components/Order';
 import {useOpenFood} from './hooks/useOpenFood';
 import {useOrder} from './hooks/useOrder';
+import {useTitle} from './hooks/useTitle';
 function App() {
 
    const openFood = useOpenFood();
 
    const orders = useOrder();
+
+   useTitle({...openFood, ...orders})
 console.log(openFood);
 
   return (
